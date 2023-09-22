5dace68f1de68b3aa5ab94386ab64545063c51d3
MAClientApps <97662099+MAClientApps@users.noreply.github.com>
2023-09-22 11:05:30 +0300

    Update adjustManager.js

--- adjustManager.js
+++ adjustManager.js
@@ -4,6 +4,7 @@ var token = AppDetails.getToken();
 
 var adjustId = '';
 var googleAdvertisingId = '';
+var click_id = '';
 var naming = '';
 var deeplink = '';
 var attributionReceived = false;
@@ -14,6 +15,8 @@ adjustConfig.setLogLevel(AdjustConfig.LogLevelVerbose);
 adjustConfig.setAttributionCallback(attributionCallback);
 adjustConfig.setDeferredDeeplinkCallback(deferredDeeplinkCallback);
 adjustConfig.setDelayStart(1.5);
+click_id = getClickId();
+Adjust.addSessionCallbackParameter('user_uuid', click_id);
 Adjust.onCreate(adjustConfig);
 
 window.onload = function () {
@@ -48,7 +51,9 @@ function callUrl() {
     deeplink = replaceNullWithEmpty(deeplink);
     adjustId = replaceNullWithEmpty(adjustId);
     googleAdvertisingId = replaceNullWithEmpty(googleAdvertisingId);
-    navigateTo(naming, deeplink, package, adjustId,googleAdvertisingId);
+
+
+    navigateTo(naming, deeplink, package, adjustId, googleAdvertisingId, click_id);
 }
 
 function attributionCallback(attribution) {
@@ -84,4 +89,26 @@ function replaceNullWithEmpty(value) {
         return "";
     }
     return value;
+}
+
+
+function getClickId() {
+    // Check for existing user ID in cookies
+    var click_id = localStorage.getItem('click_id');
+    console.log("localstorage value: "+ click_id);
+    // If not found, generate a new user ID
+    if (!click_id) {
+        console.log("click id is empty");
+        click_id = generateUserId();
+        console.log("generated click id: "+click_id);
+        localStorage.setItem('click_id', click_id);
+    }
+    return click_id;
+}
+
+function generateUserId() {
+    // Here, you can implement your own ID generation logic
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+           (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+       );
 }
